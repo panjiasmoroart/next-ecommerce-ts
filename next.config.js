5ac6e0974fcbc0b7682516c298f740ef3d783409
panjiasmoroart <pandjiasmoroart@gmail.com>
2021-10-25 23:01:13 +0700

    Section_11_Reusable_Configuration: 63 Next config file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  i18n: {
+    locales: ["en-US", "es"],
+    defaultLocale: "en-US",
+  },
+};
+
+console.log("next.config.js", JSON.stringify(module.exports, null, 2));
