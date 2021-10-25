aabb524279ed85e5aeaaa848b926b327cc97646e
panjiasmoroart <pandjiasmoroart@gmail.com>
2021-10-26 00:34:37 +0700

    Section_11_Reusable_Configuration: 66 Format JSON in config file

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const { withFrameworkConfig } = require("./framework/common/config");
 /** @type {import('next').NextConfig} */
 module.exports = withFrameworkConfig({
   reactStrictMode: true,
+  framework: {
+    name: "shopify",
+  },
   i18n: {
     locales: ["en-US", "es"],
     defaultLocale: "en-US",
