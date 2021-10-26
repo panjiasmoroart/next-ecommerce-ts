349ca83e811d2934856bf02d35934da5d55703ce
panjiasmoroart <pandjiasmoroart@gmail.com>
2021-10-26 07:24:13 +0700

    Section_11_Reusable_Configuration: 69 Api config type

---
+++ framework/common/types/api.ts
@@ -0,0 +1,21 @@
+export type ApiFetcherOptions = {
+  url: string
+  query: string
+}
+
+export type ApiFetcherResults<T> = {
+  data: T
+}
+
+export interface ApiConfig {
+  apiUrl: string 
+  // before
+  // fetch<T>(
+  //   options: ApiFetcherOptions
+  // ): Promise<{data: T}>
+  // after 
+  fetch<T>(
+    options: ApiFetcherOptions
+  ): Promise<ApiFetcherResults<T>>
+}
+
