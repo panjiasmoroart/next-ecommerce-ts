8bff423e6326869d89ac6f5e1d9336e7ff680110
panjiasmoroart <pandjiasmoroart@gmail.com>
2021-10-26 14:35:11 +0700

    Section_13_Tailwind_&_PostCSS: 81 Css variables

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,11 +1,11 @@
-import Home from "pages";
 import { FC } from "react";
 import style from "./Layout.module.css";
 
 const Layout: FC = ({ children }) => {
   return (
     <div className={style.root}>
-      <main className="fit">
+      <main style={{ color: "var(--primary)"}} 
+        className="fit">
         {children}
       </main>
     </div>
