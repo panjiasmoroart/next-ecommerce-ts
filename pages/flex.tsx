0f3ca3bc12fb516c737ae0876a050679515a3e04
panjiasmoroart <pandjiasmoroart@gmail.com>
2021-10-27 18:26:22 +0700

    Section_16_[Optional]_Flex_ Stylings: 102 Apply Flex & Flex 1

--- pages/flex.tsx
+++ pages/flex.tsx
@@ -2,7 +2,7 @@ import style from "./flex.module.css";
 
 const Flex = () => {
   return (
-    <div className="parent">
+    <div className={style.parent}>
       <div className={style.child}></div>
       <div className={style.child}></div>
       <div className={style.child}></div>
