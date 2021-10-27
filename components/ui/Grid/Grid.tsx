b22fde2ed661117fa353833fa480ce8e0bdd900f
panjiasmoroart <pandjiasmoroart@gmail.com>
2021-10-27 13:28:21 +0700

    Section_14_Grid_&_Products: 92 LayoutA

--- components/ui/Grid/Grid.tsx
+++ components/ui/Grid/Grid.tsx
@@ -1,9 +1,22 @@
 import { FC, ReactNode } from "react"
 import style from "./Grid.module.css";
+import cn from "classnames";
+interface Props {
+  children: ReactNode[]
+  layout?: "A" | "B"
+}
 
-const Grid: FC<ReactNode> = ({ children }) => {
+const Grid: FC<Props> = ({ children, layout = "A" }) => {
+  const rootClassName = cn(
+    style.root,
+    {
+      [style.layoutA]: layout === "A",
+      [style.layoutB]: layout === "B"
+    }
+  )
+  
   return(
-    <div className={style.root}>
+    <div className={rootClassName}>
       {children}
     </div>
   )
