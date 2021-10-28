51e288b1c7487b3f328c97b51659a2dc3d7dc437
panjiasmoroart <pandjiasmoroart@gmail.com>
2021-10-28 23:36:08 +0700

    Section_17_Components_Home_Page: 120 Usernav

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import { FC } from "react"
 import { Container } from "@components/ui"
 import Link from "next/link"
 import style from "./Navbar.module.css"
+import { Usernav } from "@components/common"
 
 const Navbar: FC = () => {
   return (
@@ -27,6 +28,10 @@ const Navbar: FC = () => {
               <a className={style.link}>Shoes</a>
             </Link>
           </nav>
+
+          <div className="flex flex-1 justify-end space-x-8">
+            <Usernav />
+          </div>
         </div>
       </div>
     </Container>
