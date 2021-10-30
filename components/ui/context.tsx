7ba4619778225784e049d61a7d83a7939de019c0
panjiasmoroart <pandjiasmoroart@gmail.com>
2021-10-30 21:36:23 +0700

    Section_18_UI_Provider_&_Sidebar: 126 use reducer

--- components/ui/context.tsx
+++ components/ui/context.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, FC, useState } from "react"
+import { createContext, useContext, FC, useReducer } from "react"
 
 export interface StateModifiers {
   openSidebar: () => void
@@ -23,15 +23,36 @@ const UIContext = createContext<State>({
   ...initialState
 })
 
+type Action = { type: "OPEN_SIDEBAR" | "CLOSE_SIDEBAR"}
+
+function uiReducer(state: StateValues, action: Action) {
+  switch(action.type) {
+    case "OPEN_SIDEBAR": {
+      return {
+        ...state,
+        isSidebarOpen: true
+      }
+    }
+    case "CLOSE_SIDEBAR": {
+      return {
+        ...state,
+        isSidebarOpen: false
+      }
+    }
+  }
+}
+
 export const UIProvider: FC = ({ children }) => {
+  const [state, dispatch] = useReducer(uiReducer, initialState)
   
-  const openSidebar = () => alert("Opening Sidebar");
-  const closeSidebar = () => alert("Closing Sidebar");
+  const openSidebar = () => dispatch({type: "OPEN_SIDEBAR"});
+  const closeSidebar = () => dispatch({type: "CLOSE_SIDEBAR"});
 
   const value = {
+    // isSidebarOpen: state.isSidebarOpen, 
+    ...state,
     openSidebar,
     closeSidebar,
-    isSidebarOpen: false
   }
 
 
