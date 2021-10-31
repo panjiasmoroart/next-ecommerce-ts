e44da9d17af4628587544c65b49071362b5d4458
panjiasmoroart <pandjiasmoroart@gmail.com>
2021-10-31 23:55:15 +0700

    Section_20_Normalization: 139 Normalize Options

--- framework/shopify/utils/normalize.ts
+++ framework/shopify/utils/normalize.ts
@@ -1,7 +1,8 @@
 import { 
   ImageEdge,
   MoneyV2,
-  Product as ShopifyProduct 
+  Product as ShopifyProduct, 
+  ProductOption
 } from "../schema";
 
 import { Product } from "@common/types/product";
@@ -31,6 +32,18 @@ const normalizeProductPrice = ({ currencyCode, amount }: MoneyV2) => ({
   currencyCode
 })  
 
+const normalizeProductOption = ({
+  id, 
+  values, 
+  name: displayName
+}: ProductOption) => {
+  console.log("ID", id)
+  console.log("NAME", displayName)
+  console.log("VALUES", values)
+
+  return {}
+}
+
 export function normalizeProduct(productNode: ShopifyProduct): Product {
   const {
     id,
@@ -40,6 +53,7 @@ export function normalizeProduct(productNode: ShopifyProduct): Product {
     description,
     images: imageConnection,
     priceRange,
+    options,
     ...rest
   } = productNode
 
@@ -52,6 +66,9 @@ export function normalizeProduct(productNode: ShopifyProduct): Product {
     slug: handle.replace(/^\/+|\/+$/g, ""),
     images: normalizeProductImages(imageConnection),
     price: normalizeProductPrice(priceRange.minVariantPrice),
+    options: options ? 
+      options.filter(o => o.name !== "Title") 
+             .map(o => normalizeProductOption(o)) : [],
     ...rest
   }
 
