47274c1e5215689cdaf11c974e6c4b1f038e629b
panjiasmoroart <pandjiasmoroart@gmail.com>
2021-11-02 22:57:53 +0700

    Section_23_Mutation Hook: 165. Core config

--- framework/common/index.tsx
+++ framework/common/index.tsx
@@ -1,9 +1,9 @@
-import { createContext, ReactNode, useContext } from "react";
+import { createContext, ReactNode, useContext, useMemo } from "react";
 import { ApiConfig } from "./types/api";
 
 interface ApiProviderProps {
   children: ReactNode | ReactNode[]
-  config: ApiConfig | {testKey: string}
+  config: ApiConfig 
 }
 
 export const ApiContext = createContext({})
@@ -12,8 +12,15 @@ export const ApiProvider = ({
   children,
   config
 }: ApiProviderProps) => {
+
+  const coreConfig = useMemo(() => {
+    return {
+      fetcher: config.fetch
+    }
+  }, [config.fetch])
+
   return (
-    <ApiContext.Provider value={config}>
+    <ApiContext.Provider value={coreConfig}>
       {children}
     </ApiContext.Provider>
   )
--- framework/shopify/index.tsx
+++ framework/shopify/index.tsx
@@ -13,7 +13,7 @@ interface ShopifyApiProviderProps {
 
 export const ApiProvider = ({children}: ShopifyApiProviderProps) => {
   return (
-    <CoreApiProvider config={{...config, testKey: "testValue"}}>
+    <CoreApiProvider config={{...config}}>
       {children}
     </CoreApiProvider>
   )
