edd0178de903c907711ea4008ec5a350faad6213
panjiasmoroart <pandjiasmoroart@gmail.com>
2021-11-02 23:19:00 +0700

    Section_23_Mutation Hook: 166. Api Hooks

--- framework/common/index.tsx
+++ framework/common/index.tsx
@@ -1,23 +1,26 @@
 import { createContext, ReactNode, useContext, useMemo } from "react";
-import { ApiConfig } from "./types/api";
+import { ApiConfig, ApiHooks } from "./types/api";
 
 interface ApiProviderProps {
   children: ReactNode | ReactNode[]
   config: ApiConfig 
+  hooks: ApiHooks
 }
 
 export const ApiContext = createContext({})
 
 export const ApiProvider = ({
   children,
-  config
+  config,
+  hooks
 }: ApiProviderProps) => {
 
   const coreConfig = useMemo(() => {
     return {
-      fetcher: config.fetch
+      fetcher: config.fetch,
+      hooks
     }
-  }, [config.fetch])
+  }, [config.fetch, hooks])
 
   return (
     <ApiContext.Provider value={coreConfig}>
--- framework/common/types/api.ts
+++ framework/common/types/api.ts
@@ -22,3 +22,8 @@ export interface ApiConfig {
   ): Promise<ApiFetcherResults<T>>
 }
 
+export interface ApiHooks {
+  cart: {
+    useAddItem: any
+  }
+}
