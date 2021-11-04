4bc69a451b4d3f8eb76db3739b788593a7bca8f6
panjiasmoroart <pandjiasmoroart@gmail.com>
2021-11-04 21:42:40 +0700

    Section_25_Create Cart: 189. extract data from checkout create

--- framework/shopify/utils/create-checkout.ts
+++ framework/shopify/utils/create-checkout.ts
@@ -1,15 +1,18 @@
 import { ApiFetcher } from "@common/types/api"
+import { CheckoutCreatePayload, Checkout, Maybe } from "@framework/schema"
 import { checkoutCreateMutation } from "./mutations"
 
 const createCheckout = async (
-  fetch: ApiFetcher
-): Promise<any> => {
-  debugger
+  fetch: ApiFetcher<{checkoutCreate: CheckoutCreatePayload}>
+): Promise<Maybe<Checkout | undefined>> => {
   const { data } = await fetch({
     query: checkoutCreateMutation
   })
 
-  debugger
-  return data
+  const { checkout } = data.checkoutCreate 
+  const checkoutId = checkout?.id
+  debugger  
+
+  return checkout
 }
 export default createCheckout
